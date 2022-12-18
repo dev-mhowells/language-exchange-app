fc2e7271753d3aa44502598f110cf4d6739785dd
Michael <dev.mhowells@gmail.com>
2022-12-18 14:15:21 +0000

    added css and html template for profile header in mobile view - name, location, edit button, language and rating

--- frontend/src/routes/user.js
+++ frontend/src/routes/user.js
@@ -22,6 +22,30 @@ export default function User() {
         <main className="profile-main">
             <div className="profile-container profile-header">
                 <div className="profile-banner"></div>
+                <div className="profile-content">
+                    <div className="profile-pic-container"></div>
+                    <div className="name-location-container">
+                        <h2>Brian Smelter</h2>
+                        <div>
+                            <span className="location">London, UK</span>
+                        </div>
+                    </div>
+                    <div className="edit-btn-container">
+                        <button className="edit-btn">edit</button>
+                    </div>
+                    <div className="languages-container">
+                        <div className="language-rating">
+                            <p>English</p>
+                            <div className="rating">
+                                <span className="level level-1"></span>
+                                <span className="level level-2"></span>
+                                <span className="level level-3"></span>
+                                <span className="level level-4"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="friend-messave-container"></div>
+                </div>
             </div>
             <div className="profile-container profile-trust-container"></div>
             <div className="profile-container profile-about-container">
