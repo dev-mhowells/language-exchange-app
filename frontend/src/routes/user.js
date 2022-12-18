d6006347718cda8a777b85912b5b6b6a05db359f
Michael <dev.mhowells@gmail.com>
2022-12-18 15:10:30 +0000

    mobile layout for first iteration of profile header complete

--- frontend/src/routes/user.js
+++ frontend/src/routes/user.js
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react"
+import editIcon from "../images/edit-icon.png"
+import friendIcon from "../images/friend-icon.png"
+import messageIcon from "../images/message-icon.png"
 
 export default function User() {
 
@@ -23,7 +26,9 @@ export default function User() {
             <div className="profile-container profile-header">
                 <div className="profile-banner"></div>
                 <div className="profile-content">
-                    <div className="profile-pic-container"></div>
+                    <div className="profile-pic-container">
+                        <div className="profile-pic"></div>
+                    </div>
                     <div className="name-location-container">
                         <h2>Brian Smelter</h2>
                         <div>
@@ -31,7 +36,7 @@ export default function User() {
                         </div>
                     </div>
                     <div className="edit-btn-container">
-                        <button className="edit-btn">edit</button>
+                        <button className="edit-btn"><img src={editIcon}></img></button>
                     </div>
                     <div className="languages-container">
                         <div className="language-rating">
@@ -43,8 +48,20 @@ export default function User() {
                                 <span className="level level-4"></span>
                             </div>
                         </div>
+                        <div className="language-rating">
+                            <p>Korean</p>
+                            <div className="rating">
+                                <span className="level level-1"></span>
+                                <span className="level level-2"></span>
+                                <span className="level level-3"></span>
+                                <span className="level level-4"></span>
+                            </div>
+                        </div>
                     </div>
-                    <div className="friend-messave-container"></div>
+                    <div className="friend-message-container">
+                        <button className="add-friend-btn"><img src={friendIcon}></img></button>
+                        <button className="message-btn"><img src={messageIcon}></img></button>
+                    </div>
                 </div>
             </div>
             <div className="profile-container profile-trust-container"></div>
