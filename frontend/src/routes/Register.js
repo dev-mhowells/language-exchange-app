e1912b02615b2cd3de08aa87241c4e9a0c7ab702
Michael <dev.mhowells@gmail.com>
2022-12-20 17:45:22 +0000

    added additional form fields to register form, not yet functional, added basic styling to register page

--- frontend/src/routes/Register.js
+++ frontend/src/routes/Register.js
@@ -17,28 +17,68 @@ export default function Register() {
 
     return(
         <div className="register-page">
-            <Nav />
-        <form onSubmit={(e) => {handleSubmit(e)}}>
-            <label>
-                email:
-                <input 
-                type='text' 
-                name='email'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}></input>
-            </label>
-            <label>
-                password:
-                <input 
-                type='text' 
-                name='password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}></input>
-            </label>
-            {/* prevent form from being submitted while register is happening: */}
-            <button type="submit" disabled={isLoading}>register</button>
-            {error && <div>{error}</div>}
-        </form>
+            <form onSubmit={(e) => {handleSubmit(e)}} className="register-form">
+                <h2>Create account</h2>
+                <label>
+                    Email:
+                    <input 
+                    type='text' 
+                    name='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}></input>
+                </label>
+                <label>
+                    Password:
+                    <input 
+                    type='text' 
+                    name='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}></input>
+                </label>
+                <label>
+                    Name:
+                    <input 
+                    type='text' 
+                    name='name'
+                    ></input>
+                </label>
+                <label>
+                    Age:
+                    <input 
+                    type='text' 
+                    name='age'
+                    ></input>
+                </label>
+                <label>
+                    Location:
+                    <input 
+                    type='text' 
+                    name='location'
+                    ></input>
+                </label>
+                <label className="languages-label">
+                    Languages:
+                    <div className="languages">
+                        <input 
+                        type='text' 
+                        name='lanaguage'
+                        ></input>
+                        <input 
+                        type='text' 
+                        name='level'
+                        ></input>
+                    </div>
+                </label>
+                <label className="about-label">
+                    About:
+                    <textarea />
+                </label>
+                {/* prevent form from being submitted while register is happening: */}
+                <button type="submit" disabled={isLoading} className="register-btn">register</button>
+                {error && <div>{error}</div>}
+                <p className="link-text">Already have an account? Login</p>
+            </form>
+            <div className="register-display-right"></div>
         </div>
     )
 }
