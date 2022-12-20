681ee689b9e44675dda521804a29dedd6cfe7a84
Michael <dev.mhowells@gmail.com>
2022-12-20 11:30:27 +0000

    created useLogin custom hook, very similar to useRegister, added login ability to login button on login page

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogin = () => {
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+    const {dispatch} = useAuthContext()
+
+    // to be used on the submit of register form
+    // email and password params will come straight from user input
+    const login = async (email, password) => {
+        setIsLoading(true)
+        // resets error every call
+        setError(null)
+
+        // need to pass in options because POST req
+        // fetch address does not need to match react routing!
+        const response = await fetch('/user/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            // pass in the email password object in JSON format
+            // this will be the req.body of user_controller.registerUser
+            // registerUser() in userController then responds with {email, token}
+            body: JSON.stringify({email, password})
+        })
+        // this will either return json with token or err msg
+        const json = await response.json()
+
+        if(!response.ok) {
+            setIsLoading(false)
+            setError(json.error)
+        }
+        if(response.ok) {
+            // save user to local storage (json web token and email)
+            // have to restringify the json because it needs to be stored
+            // as a string in local storage
+            localStorage.setItem('user', JSON.stringify(json))
+
+            // update auth context so that entire app knows user is logged in
+            dispatch({type: 'LOGIN', payload: json})
+
+            setIsLoading(false)
+        }
+    }
+    return {login, isLoading, error}
+} 
--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,13 +1,17 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 export default function Login() {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const {login, error, isLoading} = useLogin()
 
     // async because needs to interact with backend
     const handleSubmit =  async(e) => {
         e.preventDefault();
+
+        await login(email, password)
     }
 
     return(
@@ -28,7 +32,8 @@ export default function Login() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}></input>
             </label>
-            <button type="submit">log in</button>
+            <button type="submit" disabled={isLoading}>log in</button>
+            {error && <div>{error}</div>}
         </form>
     )
 }
