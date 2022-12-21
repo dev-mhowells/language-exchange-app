1ac4ce49d99583631a56ef7adfad823c5798e7ea
Michael <dev.mhowells@gmail.com>
2022-12-21 16:55:57 +0000

    modified name of first test, tests whether token is returned from users/register. added test for checking user is added to database from user/register

--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -16,14 +16,15 @@ before((done) => {
     done()
 })
 
-// after((done) => {
-//     User.deleteMany({}, function(err) {})
-//     done()
-//     mongoose.connection.close()
-// })
+after((done) => {
+    User.deleteMany({}, function(err) {})
+    done()
+    mongoose.connection.close()
+})
 
-describe('UserController', function() {
-    it('successfully registers a user', function(done) {
+describe('POST /register', () => {
+
+    it('returns an email and a token', (done) => {
 
         const user = {
             email: 'testemail1@email.com',
@@ -35,7 +36,33 @@ describe('UserController', function() {
         .send(user)
         .end((err, res) => {
             expect(res.status).to.equal(200)
+            expect(res.body.email).to.equal('testemail1@email.com')
+            expect(res.body.token).to.not.equal('undefined')
             done()
+        })
+
+    })
+
+    it('adds a user to the database', (done) => {
+
+        const user = {
+            email: 'testemail2@email.com',
+            password: "something",
+        }
+
+        chai.request(app)
+        .post('/user/register')
+        .send(user)
+        .end((err, res) => {
+            expect(res.status).to.equal(200)
+            
+            User.findOne({'email': 'testemail2@email.com'}, 
+            'email password',
+            (err, foundUser) => {
+                expect(foundUser.email).to.equal('testemail2@email.com')
+                expect(foundUser.password).to.not.equal('undefined')
+                done()
+            })
         })
 
     })
