484a91ae4c93d476250129dba0df8d38aa8c2fe4
Michael <dev.mhowells@gmail.com>
2023-01-14 12:19:06 +0000

    users can now write a language and select a level

--- frontend/src/routes/Register.js
+++ frontend/src/routes/Register.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useRegister } from '../hooks/useRegister'
 import { Link } from "react-router-dom"
+import plusIcon from '../images/plus-icon.png'
 import Nav from "../components/Nav";
 
 export default function Register() {
@@ -9,15 +10,17 @@ export default function Register() {
     const [password, setPassword] = useState("")
     const [name, setName] = useState("")
     const [age, setAge] = useState("")
-    const [location, setLocation] = useState("")
+    const [nationality, setNationality] = useState("")
     const [about, setAbout] = useState("")
+    const [languages, setLanguages] = useState({language: "", level: "Native"})
     const {register, isLoading, error} = useRegister()
 
+    console.log(languages)
 
     // async because needs to interact with backend
     const handleSubmit =  async(e) => {
         e.preventDefault();
-        await register(email, password, name, age, location, about)
+        await register(email, password, name, age, nationality, about)
     }
 
     return(
@@ -51,30 +54,44 @@ export default function Register() {
                 <label>
                     Age:
                     <input 
-                    type='text' 
+                    type='number' 
                     name='age'
+                    min='16'
+                    max='120'
                     value={age}
                     onChange={(e) => setAge(e.target.value)}></input>
                 </label>
                 <label>
-                    Location:
+                    Nationality:
                     <input 
                     type='text' 
-                    name='location'
-                    value={location}
-                    onChange={(e) => setLocation(e.target.value)}></input>
+                    name='nationality'
+                    value={nationality}
+                    onChange={(e) => setNationality(e.target.value)}>
+                    </input>
                 </label>
                 <label className="languages-label">
                     Languages:
                     <div className="languages">
                         <input 
                         type='text' 
-                        name='lanaguage'
+                        name='language'
+                        value={languages.language}
+                        onChange={(e) => setLanguages((languageObj) => ({...languageObj, language: e.target.value}))}
                         ></input>
-                        <input 
+                        <select 
+                        className="language-dropdown"
                         type='text' 
                         name='level'
-                        ></input>
+                        default='Native'
+                        onChange={(e) => setLanguages((languageObj) => ({...languageObj, level: e.target.value}))}
+                        >
+                            <option value="Native">Native</option>
+                            <option value="Advanced">Advanced</option>
+                            <option value="Intermediate">Intermediate</option>
+                            <option value="Beginner">Beginner</option>
+                        </select>
+                        {/* <button className="add-language-btn" type='button'><img src={plusIcon}></img></button> */}
                     </div>
                 </label>
                 <label className="about-label">
