18dd333286266fd6c589763515d0f1152f705af8
Michael <dev.mhowells@gmail.com>
2023-01-28 12:17:15 +0000

    altered colours and display of highlighted text when sentences are marked as correct, edited or currently selected

--- frontend/src/routes/Feed.js
+++ frontend/src/routes/Feed.js
@@ -4,6 +4,7 @@ import Nav from "../components/Nav"
 import Sidebar from "../components/Sidebar"
 import arrowRight from '../images/arrow-right.png'
 import arrowLeft from '../images/arrow-left.png'
+import checkmark from '../images/checkmark.png'
 
 const exampleEntry1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a dolor velit. Nunc ut tortor feugiat, tristique ante imperdiet, elementum risus. Quisque accumsan ultricies sapien a finibus. Vivamus aliquam orci ac urna fringilla mattis. Donec fermentum sem in viverra tincidunt. Nam ac quam quam. Curabitur hendrerit, justo quis iaculis mattis, ex nibh molestie mi, vel suscipit felis nulla ac lorem. Proin tempus convallis nulla dapibus tempus. Etiam commodo nunc ac tortor consequat ullamcorper. Nullam ullamcorper sagittis varius. Fusce quis feugiat nunc. Nam dignissim ex at libero porta, et mattis dui congue. Praesent nunc massa, dapibus sed purus sed, commodo elementum velit. Nullam convallis purus in metus aliquam tempor. Fusce facilisis tellus in nibh mollis accumsan. "
 const exampleEntry2 = "Integer interdum sapien vel justo congue aliquet. Duis leo turpis, consequat in molestie at, viverra eget tellus. Donec pulvinar egestas arcu, sit amet rutrum urna rhoncus eu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec id accumsan ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id sollicitudin ante. Ut luctus, tellus ut tincidunt interdum, sem neque iaculis magna, ut venenatis augue orci quis est. Suspendisse hendrerit lacus eget tortor ultrices, vel pulvinar quam feugiat. "
@@ -54,9 +55,11 @@ function Correction({entryText}) {
 
         if(allSentences[count].sentence !== sentenceToEdit.sentence) {
             newEntrySentences[count].edited = true
+            newEntrySentences[count].markedCorrect = false
             setEntrySentences(newEntrySentences)
         } else {
             newEntrySentences[count].edited = false
+            newEntrySentences[count].markedCorrect = true
             setEntrySentences(newEntrySentences)
             }
     }
@@ -70,14 +73,17 @@ function Correction({entryText}) {
     const correctedEntryDisplay = entrySentences?.map((sentenceObj, index) => {
 
         let color = 'transparent'
-        if (sentenceObj.edited === true) color = 'orange'
-        if (index === count) color = 'yellow'
+        if (sentenceObj.edited === true) color = '#FFF4CD'
+        if(sentenceObj.markedCorrect === true) color = '#C5FFAA'
+        // if (sentenceObj.edited === false && count > index) color = 'green'
+        if (index === count) color = '#FFF859'
 
         const styles = {
             backgroundColor: color,
             display: 'inline',
-            borderRadius: '.8em',
-            padding: '.3em 0.1em'
+            // borderRadius: '.6em',
+            padding: '.3em 0.1em',
+            boxShadow: '0 0 .3em .3em white inset'
         }
 
         return <p style={styles}>{sentenceObj.sentence}{`. `}</p>
@@ -91,8 +97,7 @@ function Correction({entryText}) {
             </div>
             <div className="slider">
                 <button onClick={countDown}><img src={arrowLeft}></img></button>
-                <button onClick={() => saveEdit(count)}>save</button>
-                <button onClick={() => saveAndContinue()}><img src={arrowRight}></img></button>
+                <button onClick={() => saveAndContinue()}><img src={checkmark}></img></button>
             </div>
             <div className="entry-correction">
                 {correctedEntryDisplay}
